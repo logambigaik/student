b82da97697eb3228e86f9daf55fe044d997088e1
logambigaik <54719289+logambigaik@users.noreply.github.com>
2020-01-21 19:36:13 +0530

    django

---
+++ classes.py
@@ -0,0 +1,9 @@
+class Point:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
+
+p = Point(6,4)
+print("X=",p.x)
+print("Y=",p.y)
---
+++ modules.py
@@ -0,0 +1,9 @@
+from functions import square
+
+print("Calling functions:")
+print("Square of 10:",square(10))
+
+from function1 import square
+
+print("Calling function1:")
+print("Square of 10:",square(10))
